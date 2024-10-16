96e482d0b21d982d33800fac135308a9f8e2ae0a
Yukari Hafner <shinmera@tymoon.eu>
2024-10-16 13:08:32 +0200

    Fix compile-time definition order in tests

--- test.lisp
+++ test.lisp
@@ -10,46 +10,47 @@
 (define-test bmp
   :defun T)
 
-(defvar *here* #.(make-pathname :name NIL :type NIL :defaults (or *compile-file-pathname* *load-pathname*)))
+(eval-when (:compile-toplevel :load-toplevel :execute)
+  (defvar *here* #.(make-pathname :name NIL :type NIL :defaults (or *compile-file-pathname* *load-pathname*)))
 
-(defun test-file (file)
-  (merge-pathnames file (merge-pathnames "test/" *here*)))
+  (defun test-file (file)
+    (merge-pathnames file (merge-pathnames "test/" *here*)))
 
-(defstruct (image-data (:conc-name NIL))
-  width height channels pixels)
+  (defstruct (image-data (:conc-name NIL))
+    width height channels pixels)
 
-(defun png-data (file)
-  (let ((png (pngload:load-file file :flatten T)))
-    (make-image-data
-     :pixels (pngload:data png)
-     :width (pngload:width png)
-     :height (pngload:height png)
-     :channels (ecase (pngload:color-type png)
-                 (:greyscale 1)
-                 (:greyscale-alpha 2)
-                 (:truecolour 3)
-                 (:truecolour-alpha :rgba)
-                 (:indexed-colour
-                  (truncate (length (pngload:data png))
-                            (* (pngload:width png) (pngload:height png))))))))
-
-(defun bmp-data (file)
-  (let ((bmp (bmp:read-bmp file)))
-    (multiple-value-bind (data width height channels) (bmp:decode-pixels bmp)
+  (defun png-data (file)
+    (let ((png (pngload:load-file file :flatten T)))
       (make-image-data
-       :pixels data
-       :width width
-       :height height
-       :channels channels))))
+       :pixels (pngload:data png)
+       :width (pngload:width png)
+       :height (pngload:height png)
+       :channels (ecase (pngload:color-type png)
+                   (:greyscale 1)
+                   (:greyscale-alpha 2)
+                   (:truecolour 3)
+                   (:truecolour-alpha :rgba)
+                   (:indexed-colour
+                    (truncate (length (pngload:data png))
+                              (* (pngload:width png) (pngload:height png))))))))
 
-(defun bin-diff (exp got)
-  (cond ((< (length got) (length exp)) :too-short)
-        ((< (length exp) (length got)) :too-long)
-        (T (loop for i from 0 below (length exp)
-                 for e = (aref exp i)
-                 for g = (aref got i)
-                 do (when (/= e g)
-                      (return (list :diff i :expected e :got g)))))))
+  (defun bmp-data (file)
+    (let ((bmp (bmp:read-bmp file)))
+      (multiple-value-bind (data width height channels) (bmp:decode-pixels bmp)
+        (make-image-data
+         :pixels data
+         :width width
+         :height height
+         :channels channels))))
+
+  (defun bin-diff (exp got)
+    (cond ((< (length got) (length exp)) :too-short)
+          ((< (length exp) (length got)) :too-long)
+          (T (loop for i from 0 below (length exp)
+                   for e = (aref exp i)
+                   for g = (aref got i)
+                   do (when (/= e g)
+                        (return (list :diff i :expected e :got g))))))))
 
 (defmacro define-test-for-file (file)
   `(define-test ,file
